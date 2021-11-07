469742587b19d72e143e2e5e50fece3de5762a3d
area268457913 <area268457913@hotmail.com>
2021-11-07 18:12:09 +0800

    add message partials

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const routes = require('./routes')
-
+const flash = require('connect-flash')
 
 
 const app = express()
 const PORT = 3000
 
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
