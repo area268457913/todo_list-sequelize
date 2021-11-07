58d4ec17d83e7d77b17fefb255efe05e9a8628e9
area268457913 <area268457913@hotmail.com>
2021-11-07 18:04:13 +0800

    refactor express router

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 const session = require('express-session')
 const usePassport = require('./config/passport')
-const passport = require('passport')
 const routes = require('./routes')
 
 
@@ -34,103 +29,7 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-})
 
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
-
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.get('/:id/edit', (req, res) => {
-  const UserId = req.user.id
-  const id = req.params.id
-
-  return Todo.findOne({ where: { id, UserId } })
-    .then(todo => res.render('edit', { todo: todo.get() }))
-    .catch(error => console.log(error))
-})
-app.put('/:id', (req, res) => {
-  const UserId = req.user.id
-  const id = req.params.id
-  const { name, isDone } = req.body
-  console.log(req.body)
-
-  return Todo.findOne({ where: { id, UserId } })
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.get('/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/', (req, res) => {
-  const UserId = req.user.id
-  const name = req.body.name
-
-  return Todo.create({ name, UserId })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
